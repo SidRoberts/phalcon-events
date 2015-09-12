6b858db9881f8d9e63e7578dec084722a59113a5
Sid Roberts <sid@sidroberts.co.uk>
2015-09-12 08:59:50 +0100

    Json responses are now UTF-8.

--- src/Dispatcher/JsonResponse.php
+++ src/Dispatcher/JsonResponse.php
@@ -9,7 +9,7 @@ class JsonResponse extends \Phalcon\Mvc\User\Plugin
         $data = $dispatcher->getReturnedValue();
 
         $this->response->setJsonContent($data, JSON_PRETTY_PRINT);
-        $this->response->setContentType("application/json");
+        $this->response->setContentType("application/json", "UTF-8");
         $this->response->setHeader('Cache-Control', 'private, max-age=0, must-revalidate');
 
         $this->response->send();
