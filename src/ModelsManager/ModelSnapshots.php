dcf0ad62d9a8ef3a1f617184a6b50e963014c90d
Sid Roberts <sid@sidroberts.co.uk>
2016-01-23 19:36:42 +0000

    Removed pointless snapshot setter from Model Snapshot event.

--- src/ModelsManager/ModelSnapshots.php
+++ src/ModelsManager/ModelSnapshots.php
@@ -7,7 +7,5 @@ class ModelSnapshots extends \Phalcon\Mvc\User\Plugin
     public function afterInitialize(\Phalcon\Events\Event $event, \Phalcon\Mvc\Model\ManagerInterface $modelsManager, \Phalcon\Mvc\ModelInterface $model)
     {
         $modelsManager->keepSnapshots($model, true);
-
-        $model->setSnapshotData($model->toArray());
     }
 }
