62d232702832adf587e66586f37dd946fb65dc5c
Sid Roberts <sid@sidroberts.co.uk>
2016-01-31 20:06:39 +0000

    The JSON Response Dispatcher event no longer forcefully sends the Response.

--- src/Dispatcher/JsonResponse.php
+++ src/Dispatcher/JsonResponse.php
@@ -15,7 +15,5 @@ class JsonResponse extends \Phalcon\Mvc\User\Plugin
         $this->response->setJsonContent($data, JSON_PRETTY_PRINT);
         $this->response->setContentType("application/json", "UTF-8");
         $this->response->setHeader('Cache-Control', 'private, max-age=0, must-revalidate');
-
-        $this->response->send();
     }
 }
