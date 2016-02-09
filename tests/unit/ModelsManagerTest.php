559112f738df0d4c826ff3447348fe6f07c5470e
Sid Roberts <sid@sidroberts.co.uk>
2016-02-09 17:09:26 +0000

    Added Models Manager unit tests.

---
+++ tests/unit/ModelsManagerTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Sid\Phalcon\Events\Tests\Unit;
+
+class ModelsManagerTest extends \Codeception\TestCase\Test
+{
+   /**
+    * @var \UnitTester
+    */
+    protected $tester;
+
+    protected function _before()
+    {
+        \Phalcon\Di::reset();
+
+        $di = new \Phalcon\Di\FactoryDefault();
+
+        $di->set(
+            "modelsManager",
+            function () {
+                $modelsManager = new \Phalcon\Mvc\Model\Manager();
+
+                $eventsManager = new \Phalcon\Events\Manager();
+
+                $eventsManager->attach("modelsManager", new \Sid\Phalcon\Events\ModelsManager\ModelDynamicUpdate());
+                $eventsManager->attach("modelsManager", new \Sid\Phalcon\Events\ModelsManager\ModelNamespaceSource());
+                $eventsManager->attach("modelsManager", new \Sid\Phalcon\Events\ModelsManager\ModelSnapshots());
+
+                $modelsManager->setEventsManager($eventsManager);
+
+                return $modelsManager;
+            },
+            true
+        );
+
+        $this->dispatcher = $di->get("dispatcher");
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testModelDynamicUpdate()
+    {
+        $post = new \A\B\C\Posts();
+
+        $this->assertTrue(
+            $post->getModelsManager()->isUsingDynamicUpdate($post)
+        );
+    }
+
+    public function testModelNamespaceSource()
+    {
+        $post = new \A\B\C\Posts();
+
+        $this->assertEquals(
+            "A_B_C_Posts",
+            $post->getSource()
+        );
+    }
+
+    public function testModelSnapshots()
+    {
+        $post = new \A\B\C\Posts();
+
+        $this->assertTrue(
+            $post->getModelsManager()->isKeepingSnapshots($post)
+        );
+    }
+}
---
+++ tests/unit/_bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+// Here you can initialize variables that will be available to your tests
+
+namespace A\B\C {
+    class Posts extends \Phalcon\Mvc\Model
+    {
+        
+    }
+}
