9d06e2d933d3658f3c0134e16cd4b6c23e38eff3
Sid Roberts <sid@sidroberts.co.uk>
2016-06-02 13:36:42 +0100

    Ajax Response event now sets the render level to LEVEL_LAYOUT.

--- src/Dispatcher/AjaxResponse.php
+++ src/Dispatcher/AjaxResponse.php
@@ -11,7 +11,7 @@ class AjaxResponse extends \Phalcon\Mvc\User\Plugin
     public function beforeDispatchLoop(\Phalcon\Events\Event $event, \Phalcon\Mvc\DispatcherInterface $dispatcher, $data)
     {
         if ($this->request->isAjax()) {
-            $this->view->setRenderLevel(\Phalcon\Mvc\View::LEVEL_ACTION_VIEW);
+            $this->view->setRenderLevel(\Phalcon\Mvc\View::LEVEL_LAYOUT);
         }
     }
 }
