bb7ac55035f3093fb911cf86b46c08522fc355b4
Sid Roberts <sid@sidroberts.co.uk>
2016-06-05 18:26:24 +0100

    You can now change the controller/action of the Exception Handler Dispatcher event.

--- src/Dispatcher/ExceptionHandler.php
+++ src/Dispatcher/ExceptionHandler.php
@@ -5,6 +5,30 @@ namespace Sid\Phalcon\Events\Dispatcher;
 class ExceptionHandler extends \Phalcon\Mvc\User\Plugin
 {
     /**
+     * @var string
+     */
+    protected $controller;
+
+    /**
+     * @var string
+     */
+    protected $action;
+
+
+
+    /**
+     * @param string $controller
+     * @param string $action
+     */
+    public function __construct($controller = "error", $action = "exception")
+    {
+        $this->controller = $controller;
+        $this->action     = $action;
+    }
+
+
+
+    /**
      * @param \Phalcon\Events\Event        $event
      * @param \Phalcon\DispatcherInterface $dispatcher
      * @param \Exception                   $exception
@@ -15,9 +39,9 @@ class ExceptionHandler extends \Phalcon\Mvc\User\Plugin
     {
         $dispatcher->forward(
             [
-                'controller' => 'error',
-                'action'     => 'exception',
-                'params'     => [
+                "controller" => $this->controller,
+                "action"     => $this->action,
+                "params"     => [
                     $exception
                 ]
             ]
